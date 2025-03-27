819fb2be0fbc49b760721f4662f572505bc4a639
Kauan Vidigal <kauanvidigalcontato@gmail.com>
2025-03-27 01:58:55 -0300

    update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect
 from models import db, Appointment
 from datetime import datetime
-import html  # For escaping user input
+import bleach  
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointments.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'  # For flash messages
 db.init_app(app)
 
 with app.app_context():
@@ -19,30 +18,21 @@ def index():
 @app.route('/schedule', methods=['GET', 'POST'])
 def schedule_appointment():
     if request.method == 'POST':
-        name = request.form['name']
+        name = bleach.clean(request.form['name']) 
         time = request.form['time']
         date = request.form['date']
-        attendant = request.form['attendant']
-
-        # Escape the user inputs to prevent XSS
-        name = html.escape(name)
-        attendant = html.escape(attendant)
-
-        try:
-            new_appointment = Appointment(
-                name=name,
-                time=datetime.strptime(time, '%H:%M').time(),
-                date=datetime.strptime(date, '%Y-%m-%d').date(),
-                attendant=attendant
-            )
-
-            db.session.add(new_appointment)
-            db.session.commit()
-            flash('Appointment scheduled successfully!', 'success')
-            return redirect('/appointments')
-        except Exception as e:
-            flash(f'Error scheduling appointment: {str(e)}', 'danger')
-
+        attendant = bleach.clean(request.form['attendant'])  
+        new_appointment = Appointment(
+            name=name,
+            time=datetime.strptime(time, '%H:%M').time(),
+            date=datetime.strptime(date, '%Y-%m-%d').date(),
+            attendant=attendant
+        )
+        
+        db.session.add(new_appointment)
+        db.session.commit()
+        return redirect('/appointments')
+    
     return render_template('schedule_appointment.html')
 
 @app.route('/appointments')
@@ -56,9 +46,6 @@ def delete_appointment(id):
     if appointment:
         db.session.delete(appointment)
         db.session.commit()
-        flash('Appointment deleted successfully!', 'success')
-    else:
-        flash('Appointment not found!', 'danger')
     return redirect('/appointments')
 
 if __name__ == '__main__':
